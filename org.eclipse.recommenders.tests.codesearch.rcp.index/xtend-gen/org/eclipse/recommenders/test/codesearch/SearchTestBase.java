887438b07f40694edf831bbab642eba13ac73f51
Marcel Bruch <marcel.bruch@codetrails.com>
2013-02-26 22:51:38 +0100

    [releng] added feature and .product

---
+++ org.eclipse.recommenders.tests.codesearch.rcp.index/xtend-gen/org/eclipse/recommenders/test/codesearch/SearchTestBase.java
@@ -0,0 +1,26 @@
+package org.eclipse.recommenders.test.codesearch;
+
+import org.eclipse.recommenders.codesearch.rcp.index.searcher.SearchResult;
+import org.eclipse.recommenders.test.codesearch.rcp.indexer.TestBase;
+import org.eclipse.xtend2.lib.StringConcatenation;
+import org.eclipse.xtext.xbase.lib.Exceptions;
+import org.junit.Assert;
+
+@SuppressWarnings("all")
+public class SearchTestBase extends TestBase {
+  public void assertQueryResultGtZero(final CharSequence query) {
+    try {
+      String _string = query.toString();
+      SearchResult result = this.search.lenientSearch(_string);
+      StringConcatenation _builder = new StringConcatenation();
+      _builder.append("Query [");
+      _builder.append(query, "");
+      _builder.append("] yielded no results");
+      String _string_1 = _builder.toString();
+      boolean _greaterThan = (result.docs.totalHits > 0);
+      Assert.assertTrue(_string_1, _greaterThan);
+    } catch (Exception _e) {
+      throw Exceptions.sneakyThrow(_e);
+    }
+  }
+}
